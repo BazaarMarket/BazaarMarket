be8f7c29d68f389d3cd85f0c083d84ec0f9874a4
root <root@Macs-Blade-2016.localdomain>
2021-04-17 18:38:39 -0600

    Adds Profile Info to Profile Page

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -1,66 +1,113 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'wouter';
-import { Box, Flex, Text, Heading, Image, Link } from '@chakra-ui/react';
+import { 
+  Box,
+  Flex, 
+  Text, 
+  Heading, 
+  Image, 
+  Link,
+  Menu,
+  MenuItem, 
+  MenuButton,
+  MenuList,
+  Spacer 
+} from '@chakra-ui/react';
+import { ChevronDown, Package, Plus, GitHub, ExternalLink } from 'react-feather';
 import { MinterButton /* , MinterLink */ } from '../common';
 import logo from '../../components/common/assets/logo.svg';
-import { useSelector, useDispatch } from '../../reducer';
 import { connectWallet } from '../../reducer/async/wallet';
+import { disconnectWallet } from '../../reducer/async/wallet';
+import { useSelector, useDispatch } from '../../reducer';
 
-export default function AccountPage() {
+interface HeaderLinkProps {
+  to: string;
+  children: React.ReactNode;
+}
 
-  function WalletInfo(props: { tzPublicKey: string }) {
-    return (
-      <>
-        <Box borderRadius="100%" width="50px" height="50px" bg="white" borderWidth="1px" borderColor="lightGray">
-          <Image borderRadius="100%"
-            src={`https://services.tzkt.io/v1/avatars2/${props.tzPublicKey}`}
-          />
-        </Box>
-      </>
-    );
-  }
-  
-  function WalletDisplay() {
-    const [, setLocation] = useLocation();
-    const system = useSelector(s => s.system);
-    const dispatch = useDispatch();
-    if (system.status == 'WalletConnected') {
-      return (
-        <>
-        <Flex>
-        <WalletInfo tzPublicKey={system.tzPublicKey} />
-        </Flex>
-        </>
-      );
-    }
-  }
-
-
+function HeaderLink(props: HeaderLinkProps) {
+  const [location, setLocation] = useLocation();
+  const selected = location === props.to;
   return (
-    <Flex
-      align="center"
-      justifyContent="space-between"
-      w="100%"
-      flex="1"
-      flexDir="column"
-      bg="#ffffff"
+    <Link
+      href={props.to}
+      onClick={e => {
+        e.preventDefault();
+        setLocation(props.to);
+      }}
+      textDecor="none"
+      borderRadius="10px"
+      alignItems="center"
+      fontWeight="600"
+      //px={3}
+      //py={2}
+      mr={4}
+      //bg={selected ? 'brand.blue' : 'white'}
+      //color={selected ? 'white' : 'brand.blue'}
+      display="flex"
+      transition="none"
+      _hover={{
+        textDecor: 'none',
+        //bg: 'gray.700',
+        //color: selected ? 'gray.400' : 'gray.100'
+      }}
     >
-      
-      <Flex mt="10%" flexDir="column" align="center" maxW="600px" pt={20} height="1100px">
-        <Heading color="brand.darkGray" size="xl" pb={8}>
-          This is the account page.
-        </Heading>
-        <Heading
-          color="brand.darkGray"
-          size="md"
-          textAlign="center"
-          fontFamily="Helvetica"
-          pb={12}
-          opacity=".8"
-        >
-          More info coming soon.
-        </Heading>
-      </Flex>
+      {props.children}
+    </Link>
+  );
+}
+
+function WalletInfo(props: { tzPublicKey: string }) {
+  return (
+    <Flex width="100%" align="center" float="left" flexDir="column">
+      <Box borderRadius="100%" width="200px" height="200px" bg="white" align="center" boxShadow="2px 2px 4px rgba(0, 0, 0, .3);">
+        <Image borderRadius="100%" width="100%"
+          src={`https://services.tzkt.io/v1/avatars2/${props.tzPublicKey}`}
+        />
+      </Box>
+      <Text pt="25px" fontWeight="bold">
+        {props.tzPublicKey}
+      </Text>
     </Flex>
   );
 }
+
+function WalletDisplay() {
+  const [, setLocation] = useLocation();
+  const system = useSelector(s => s.system);
+  const dispatch = useDispatch();
+  if (system.status !== 'WalletConnected') {
+    return (
+      <>
+      <Link>
+          <MinterButton
+            variant="secondaryActionLined"
+            
+            onClick={e => {
+              e.preventDefault();
+              dispatch(connectWallet());
+            }}
+          >
+            <Text m={2} fontWeight="bold">Login</Text>
+          </MinterButton>
+      </Link>
+    </>
+    );
+  }
+  return (
+    <>
+      <WalletInfo tzPublicKey={system.tzPublicKey} />
+    </>
+  );
+}
+
+export function AccountPage() {
+  const [location, setLocation] = useLocation();  
+  return (
+    <>
+      <WalletDisplay /> 
+    </>
+  );
+}
+
+export default AccountPage;
