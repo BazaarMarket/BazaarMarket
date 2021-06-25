b90ec26acd8591821b0800c04706c2256c9e18ce
root <root@Macs-Blade-2016.localdomain>
2021-06-25 01:13:46 -0600

    3rd attempt at contact form

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -677,6 +677,33 @@ function AboutTokenomics(){
   );
 }
 
+function ContactForm(){
+  return(
+    <form name="contact" method="post" data-netlify="true">
+      <input type="hidden" name="form-name" value="pizzaOrder" />
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Your Role:  
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
+}
 
 export default function AboutPage() {
   return (
@@ -703,27 +730,7 @@ export default function AboutPage() {
         >
           Testing contact form below vvvv.
         </Heading>
-
-        <form name="contact" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <Button type="submit">Send</Button>
-  </p>
-</form>
+        <ContactForm/>
         <Flex>
         </Flex>
       </Flex>
